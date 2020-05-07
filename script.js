1290556385c209871707a165c84d79381022f095
Bhumika Amarjeeth <bamarjee@asu.edu>
2020-05-07 22:01:01 +0530

    animation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
@@ -15,41 +17,54 @@ function myFunction(x) {
 
 //clients
 
-$(document).ready(function(){
 
-if($('.brands_slider').length)
-{
-var brandsSlider = $('.brands_slider');
-brandsSlider.owlCarousel(
-{
-loop:true,
+$('.owl-carousel').owlCarousel({
+  loop: true,
 autoplay:true,
 autoplayTimeout:5000,
-nav:true,
-dots:false,
-autoWidth:true,
-items:8,
-margin:42
-});
+  margin: 6,
+  nav: true,
+  navText: [
+    '<i class="fa fa-angle-left roundBorder mr-4"></i>','<i class="fa fa-angle-right roundBorder ml-4"></i>'
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
 
-if($('.brands_prev').length)
-{
-var prev = $('.brands_prev');
-prev.on('click', function()
-{
-brandsSlider.trigger('prev.owl.carousel');
-});
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
 }
 
-if($('.brands_next').length)
-{
-var next = $('.brands_next');
-next.on('click', function()
-{
-brandsSlider.trigger('next.owl.carousel');
-});
-}
-}
-
-
-});
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
