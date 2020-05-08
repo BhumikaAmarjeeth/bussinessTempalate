4a81eb26fe5a4bbfec98e16eb039aabc75c8e453
fulgid <navin@fulgid.in>
2020-05-08 14:26:58 +0530

    This is a new commit

--- script.js
+++ script.js
@@ -9,7 +9,16 @@ $(function () {
         }
     });
 });
-
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 10) {
+      $('.navbar .navbar-brand img').attr('src','./images/logo-dark.png');
+    }
+    if ($(this).scrollTop() < 10) {
+      $('.navbar .navbar-brand img').attr('src','./images/logo.png');
+    }
+  })
+});
 // Hamburgur menu
 function myFunction(x) {
   x.classList.toggle("change");
@@ -22,7 +31,7 @@ $('.owl-carousel').owlCarousel({
   loop: true,
 autoplay:true,
 autoplayTimeout:5000,
-  margin: 6,
+  margin: 10,
   nav: true,
   navText: [
     '<i class="fa fa-angle-left roundBorder mr-4"></i>','<i class="fa fa-angle-right roundBorder ml-4"></i>'
@@ -68,3 +77,74 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+
+//Go to top button
+var btn = $('#myBtn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+//smooth scroll for menu link
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+//script fro slider
+
+$(document).ready(function(){
+  for (var i=1; i <= $('.slider__slide').length; i++){
+    $('.slider__indicators').append('<div class="slider__indicator" data-slide="'+i+'"></div>')
+  }
+  setTimeout(function(){
+    $('.slider__wrap').addClass('slider__wrap--hacked');
+  }, 1000);
+})
+
+function goToSlide(number){
+  $('.slider__slide').removeClass('slider__slide--active');
+  $('.slider__slide[data-slide='+number+']').addClass('slider__slide--active');
+}
+
+$('.slider__next, .go-to-next').on('click', function(){
+  var currentSlide = Number($('.slider__slide--active').data('slide'));
+  var totalSlides = $('.slider__slide').length;
+  currentSlide++
+  if (currentSlide > totalSlides){
+    currentSlide = 1;
+  }
+  goToSlide(currentSlide);
+});
