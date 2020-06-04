ef5aff0cc7023883e879c1d471cbf5e395617ac2
navin-nk <navin@fulgid.in>
2020-06-04 12:48:26 -0700

    Nav  links should be active  when concerned section is visible

--- js/script.js
+++ js/script.js
@@ -180,3 +180,48 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("all").style.marginRight= "0";
 }
+
+//menu scroll color change
+var lastId,
+    topMenu = $(".menu-content"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active1")
+            .end().filter("[href='#"+id+"']").parent().addClass("active1");
+    }
+});
